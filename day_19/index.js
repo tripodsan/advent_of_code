3766aaae0af50db4f718a97fa91f4ddbbb92c218
Tobias Bocanegra <tripod@adobe.com>
2019-12-19 21:08:21 +0900

    day 19

---
+++ day_19/index.js
@@ -0,0 +1,164 @@
+
+const fs = require('fs');
+
+const data = fs.readFileSync('./input.txt', 'utf-8')
+  .split(',')
+  .map((s)=>Number.parseInt(s));
+
+class Program {
+  constructor(code, input = []) {
+    this.code = code;
+    this.input = input;
+    this.pc = 0;
+    this.isRunning = true;
+    this.isWaiting = false;
+    this.output = [];
+    this.relBase = 0;
+  }
+
+  run() {
+    if (this.isWaiting && this.input.length > 0) {
+      this.isWaiting = false;
+    }
+    while (this.isRunning && !this.isWaiting) {
+      this.step();
+    }
+    return this.output;
+  }
+
+  step() {
+    const { code } = this;
+    const opc = code[this.pc];
+    const m = [
+      Math.floor(opc / 100) % 10,
+      Math.floor(opc / 1000) % 10,
+      Math.floor(opc / 10000) % 10
+    ];
+
+    const lda = (a) => {
+      const p = code[this.pc + 1 + a];
+      switch (m[a]) {
+        case 0: return code[p] || 0;
+        case 1: return p;
+        case 2: return code[p + this.relBase] || 0;
+      }
+      throw Error('illegal parameter mode: ' + m[a]);
+    };
+
+    const sta = (a, v) => {
+      const p = code[this.pc + 1 + a];
+      switch (m[a]) {
+        case 0: code[p] = v; return;
+        case 1: break;
+        case 2: code[p + this.relBase] = v; return;
+      }
+      throw Error('illegal parameter mode: ' + m[a]);
+    };
+
+    const ops = {
+      // c = a + b
+      '1': () => {
+        sta(2, lda(0) + lda(1));
+        this.pc += 4;
+      },
+      // c = a * b
+      '2': () => {
+        sta(2, lda(0) * lda(1));
+        this.pc += 4;
+      },
+      // a = input
+      '3': () => {
+        if (this.input.length === 0) {
+          this.isWaiting = true;
+          return;
+        }
+        sta(0, this.input.shift());
+        this.isWaiting = false;
+        this.pc += 2;
+      },
+      // output = a;
+      '4': () => {
+        this.output.push(lda(0));
+        this.pc += 2;
+      },
+      // if (a) jmp b
+      '5': () => {
+        if (lda(0)) {
+          this.pc = lda(1) - 3;
+        }
+        this.pc += 3;
+      },
+      // if (!a) jmp b
+      '6': () => {
+        if (!lda(0)) {
+          this.pc = lda(1) - 3;
+        }
+        this.pc += 3;
+      },
+      // c = a < b
+      '7': () => {
+        sta(2, lda(0) < lda(1) ? 1 : 0);
+        this.pc += 4;
+      },
+      // c = a == b
+      '8': () => {
+        sta(2, lda(0) === lda(1) ? 1 : 0);
+        this.pc += 4;
+      },
+      // relBase += a
+      '9': () => {
+        this.relBase += lda(0);
+        this.pc += 2;
+      },
+      // terminate
+      '99': () => {
+        this.isRunning = false;
+      }
+    };
+    const fn = ops[opc % 100];
+    if (!fn) {
+      throw Error('illegal opcode: ' + opc);
+    }
+    fn();
+  }
+}
+
+function get(x,y) {
+  return (new Program(Array.from(data), [x,y]).run())[0];
+}
+
+function puzzle1() {
+  let s = 0;
+  for (let y = 0; y<50; y++) {
+    for (let x = 0; x<50; x++) {
+      const c = get(x,y);
+      process.stdout.write(`${c}`);
+      s += c;
+    }
+    process.stdout.write('\n');
+  }
+  console.log('\nResult 1:', s);
+}
+
+function puzzle2() {
+  let x = 0;
+  let y = 0;
+  while (true) {
+    const top_right = get(x + 99, y);
+    if (!top_right) {
+      y++;
+    }
+    const bot_left = get(x, y + 99);
+    if (!bot_left) {
+      x++;
+    }
+    if (top_right * bot_left) {
+      break;
+    }
+  }
+  console.log('Result 2:', x*10000 + y);
+}
+
+puzzle1();
+puzzle2();
+
