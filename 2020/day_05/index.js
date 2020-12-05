448aeb4f719d4d858253cf971f77749676992f6a
Tobias Bocanegra <tripod@adobe.com>
2020-12-05 14:43:18 +0900

    day 05

--- 2020/day_05/index.js
+++ 2020/day_05/index.js
@@ -1,20 +1,22 @@
 
 const fs = require('fs');
 
-const data = fs.readFileSync('./input_test.txt', 'utf-8')
+const data = fs.readFileSync('./input.txt', 'utf-8')
   .split('\n')
   .map((s)=> s.trim())
-  .filter((s) => !!s);
+  .filter((s) => !!s)
+  .map((s) => s.replace(/[FL]/g, '0').replace(/[BR]/g, 1))
+  .map((s) => Number.parseInt(s, 2))
+  .sort((a,b) => a-b);
 
-function process() {
-  return 42;
-}
+// data.forEach((d) => console.log(d, typeof(d)))
+
 function puzzle2() {
-  return process();
+  return data.filter((id, idx, a) => (id+1) !== a[idx+1])[0];
 }
 
 function puzzle1() {
-  return process();
+  return data.reduce((p, c) => Math.max(p, c), 0);
 }
 
 console.log('puzzle 1: ', puzzle1());
