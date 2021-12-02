42c01c6b474d613a4ac4a227f91157b89a7e8338
Tobias Bocanegra <tripod@adobe.com>
2021-12-02 11:09:39 +0100

    day 02

--- 2018/day_02/index.js
+++ 2018/day_02/index.js
@@ -1,51 +1,51 @@
 
 const fs = require('fs');
+const A = 'a'.charCodeAt(0);
 const data = fs.readFileSync('./input.txt', 'utf-8')
   .split('\n')
   .map((s)=> s.trim())
   .filter((s) => !!s)
-  .map((s) => {
-    const [d, n] = s.split(' ');
-    return [d, parseInt(n, 10)];
-  });
-
+  .map((s) => s.split(''));
 
 function puzzle2() {
-  let pos = 0;
-  let dep = 0;
-  let aim = 0;
-  for (const [d, n] of data) {
-    if (d === 'forward') {
-      pos += n;
-      dep += aim * n;
-    }
-    if (d === 'down') {
-      aim += n;
-    }
-    if (d === 'up') {
-      aim -= n;
+  for (let i = 0; i < data.length - 1; i++) {
+    const a = data[i];
+    for (let j = i + 1; j < data.length; j++) {
+      let miss = 0;
+      const b = data[j];
+      for (let x = 0; x < a.length; x++) {
+        if (a[x] !== b[x]) {
+          miss++;
+          if (miss > 1) {
+            break;
+          }
+        }
+      }
+      if (miss === 1) {
+        return a.filter((c, idx) => a[idx] === b[idx]).join('');
+      }
     }
   }
-  console.log('pos', pos, 'dep', dep);
-  return pos * dep;
 }
 
 function puzzle1() {
-  let pos = 0;
-  let dep = 0;
-  for (const [d, n] of data) {
-    if (d === 'forward') {
-      pos += n;
+  let twos = 0;
+  let threes = 0;
+  for (const box of data) {
+    const cnt = [];
+    box
+      .map((c) => c.charCodeAt(0) - A)
+      .forEach((b) => {
+      cnt[b] = cnt[b] ? cnt[b] + 1 : 1;
+    });
+    if (cnt.indexOf(2) >= 0) {
+      twos++;
     }
-    if (d === 'down') {
-      dep += n;
-    }
-    if (d === 'up') {
-      dep -= n;
+    if (cnt.indexOf(3) >= 0) {
+      threes++;
     }
   }
-  console.log('pos', pos, 'dep', dep);
-  return pos * dep;
+  return twos * threes;
 }
 
 console.log('puzzle 1: ', puzzle1());
