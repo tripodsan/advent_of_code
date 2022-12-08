7ac1807f2e3d523dd8c7888d7ed21946c007c5b7
Tobias Bocanegra <tripod@adobe.com>
2022-12-08 11:42:07 +0100

    day 08

--- 2022/day_08/index.js
+++ 2022/day_08/index.js
@@ -12,54 +12,22 @@ const H = data[0].length;
 // console.log(W, H)
 
 function calc(x, y) {
-  const h = data[y][x];
-  let ss = 1;
-  let vv = false;
-  let s = 0;
-  let v = true;
-
-  const visit = (height) => {
-    s += 1;
-    if (height >= h) {
-      v = false;
+  const height = data[y][x];
+  let score = 1;
+  let visible = false;
+  for (const [dx, dy] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
+    let s = 0;
+    let v = true;
+    for (let xx = x + dx, yy = y + dy; v && xx >= 0 && xx < W && yy >= 0 && yy < H; xx += dx, yy += dy) {
+      s += 1;
+      if (data[yy][xx] >= height) {
+        v = false;
+      }
     }
+    score *= s ? s : 1;
+    visible |= v;
   }
-
-  // scan to the left
-  for (let xx = x - 1; v && xx >= 0; xx -= 1) {
-    visit(data[y][xx]);
-  }
-  ss *= s ? s : 1;
-  vv |= v;
-
-  // scan to the right
-  s = 0;
-  v = true;
-  for (let xx = x + 1; v && xx < W; xx += 1) {
-    visit(data[y][xx]);
-  }
-  ss *= s ? s : 1;
-  vv |= v;
-
-  // scan to the top
-  s = 0;
-  v = true;
-  for (let yy = y - 1; v && yy >= 0; yy -= 1) {
-    visit(data[yy][x]);
-  }
-  ss *= s ? s : 1;
-  vv |= v;
-
-  // scan to the bottom
-  s = 0;
-  v = true;
-  for (let yy = y + 1; v && yy < H; yy += 1) {
-    visit(data[yy][x]);
-  }
-  ss *= s ? s : 1;
-  vv |= v;
-
-  return [ss, vv];
+  return [score, visible];
 }
 
 function run() {
@@ -67,8 +35,8 @@ function run() {
   let v = 0;
   for (let y = 0; y < H; y += 1) {
     for (let x = 0; x < W; x += 1) {
-      const [sc, visible] = calc(x, y);
-      s = Math.max(s, sc);
+      const [score, visible] = calc(x, y);
+      s = Math.max(s, score);
       if (visible) {
         v += 1;
       }
