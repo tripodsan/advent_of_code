c6d14f2496c69c5cf28a83bf4f384d3467b1ff70
Tobias Bocanegra <tripod@adobe.com>
2023-12-02 21:59:48 +0100

    day 02

--- 2023/src/day_01.rs
+++ 2023/src/day_01.rs
@@ -1,61 +1,64 @@
 use regex::Regex;
 
 fn puzzle1() {
-    let re = Regex::new(r"(\d)").unwrap();
-    let content = std::fs::read_to_string("day_01/input.txt").expect("could not read file");
-    let lines:Vec<_> = content.trim().lines().collect();
-    let mut sum:u32 = 0;
-    for line in lines {
-        let mut first_digit:u32 = 0;
-        let mut last_digit:u32 = 0;
-        for digit in re.find_iter(line).map(|c| c.as_str().parse().unwrap()) {
-            last_digit = digit;
-            if first_digit == 0 {
-                first_digit = last_digit
-            }
-        }
-        sum += first_digit * 10 + last_digit;
+  let re = Regex::new(r"(\d)").unwrap();
+  let content = std::fs::read_to_string("day_01/input.txt").expect("could not read file");
+  let lines: Vec<_> = content.trim().lines().collect();
+  let mut sum: u32 = 0;
+  for line in lines {
+    let mut first_digit: u32 = 0;
+    let mut last_digit: u32 = 0;
+    for digit in re.find_iter(line).map(|c| c.as_str().parse().unwrap()) {
+      last_digit = digit;
+      if first_digit == 0 {
+        first_digit = last_digit
+      }
     }
-    println!("puzzle 1: {}", sum);
+    sum += first_digit * 10 + last_digit;
+  }
+  println!("puzzle 1: {}", sum);
 }
+
 // 54338
-fn to_digit(digit:&str) -> u32 {
-    match digit {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        _ => digit.parse::<u32>().unwrap(),
-    }
+fn to_digit(digit: &str) -> u32 {
+  match digit {
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+    _ => digit.parse::<u32>().unwrap(),
+  }
 }
 
 fn reverse(s: &str) -> String {
-    s.chars().rev().collect()
-}
-fn puzzle2() {
-    // unfortunately, RUST regexp doesn't support lookahead/overlap..so we reverse and search twice
-    let re1 = Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine)").unwrap();
-    let re2 = Regex::new(r"(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)").unwrap();
-    let content = std::fs::read_to_string("day_01/input.txt").expect("could not read file");
-    let lines:Vec<_> = content.trim().lines().collect();
-    let mut sum:u32 = 0;
-    for line in lines {
-        let first = re1.find(line).unwrap().as_str();
-        let reversed = reverse(line);
-        let last = reverse(re2.find(reversed.as_str()).unwrap().as_str());
-        let first_digit = to_digit(first);
-        let last_digit = to_digit(last.as_str());
-        sum += first_digit * 10 + last_digit;
-    }
-    println!("puzzle 2: {}", sum);
+  s.chars().rev().collect()
 }
 
+fn puzzle2() {
+  // unfortunately, RUST regexp doesn't support lookahead/overlap..so we reverse and search twice
+  let re1 = Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+  let re2 = Regex::new(r"(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)").unwrap();
+  let content = std::fs::read_to_string("day_01/input.txt").expect("could not read file");
+  let lines: Vec<_> = content.trim().lines().collect();
+  let mut sum: u32 = 0;
+  for line in lines {
+    let first = re1.find(line).unwrap().as_str();
+    let reversed = reverse(line);
+    let last = reverse(re2.find(reversed.as_str()).unwrap().as_str());
+    let first_digit = to_digit(first);
+    let last_digit = to_digit(last.as_str());
+    sum += first_digit * 10 + last_digit;
+  }
+  println!("puzzle 2: {}", sum);
+}
+
+#[allow(dead_code)]
 pub fn run() {
-    puzzle1();
-    puzzle2();
+  puzzle1();
+  puzzle2();
 }
--- 2023/src/day_02.rs
+++ 2023/src/day_02.rs
@@ -3,59 +3,63 @@ use regex::Regex;
 
 #[derive(Debug)]
 struct Game {
-    id:u32,
-    samples:Vec<UVec3>
+  id: u32,
+  samples: Vec<UVec3>,
 }
 
-fn load_data()->Vec<Game> {
-    // Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-    let re1 = Regex::new(r"Game (\d+):(.+)").unwrap();
-    let content = std::fs::read_to_string("day_02/input.txt").expect("could not read file");
-    let lines:Vec<_> = content.trim().lines().collect();
-    let mut games:Vec<Game> = Vec::new();
-    for line in lines {
-        let caps = re1.captures(line).unwrap();
-        let id:u32 = caps.get(1).map_or(0, |m| m.as_str().parse().unwrap());
-        let right = caps.get(2).map_or("", |m| m.as_str());
-        let game = Game {
-            id,
-            samples: right.trim().split(';').map(|samp| {
-                let mut sample = UVec3::new(0, 0, 0);
-                for pt in samp.trim().split(',') {
-                    if let Some((num, color)) = pt.trim().split_once(' ') {
-                        let v = num.parse().unwrap();
-                        match color {
-                            "red" => sample.x = v,
-                            "green" => sample.y = v,
-                            "blue" => sample.z = v,
-                            _ => {}
-                        }
-                    }
-                }
-                sample
-            }).collect()
-        };
-
-
-        games.push(game);
-    }
-    games
+fn load_data() -> Vec<Game> {
+  // Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+  let re1 = Regex::new(r"Game (\d+):(.+)").unwrap();
+  let content = std::fs::read_to_string("day_02/input.txt").expect("could not read file");
+  let lines: Vec<_> = content.trim().lines().collect();
+  let mut games: Vec<Game> = Vec::new();
+  for line in lines {
+    let caps = re1.captures(line).unwrap();
+    let id: u32 = caps.get(1).map_or(0, |m| m.as_str().parse().unwrap());
+    let right = caps.get(2).map_or("", |m| m.as_str());
+    let game = Game {
+      id,
+      samples: right.trim().split(';').map(|samp| {
+        let mut sample = UVec3::new(0, 0, 0);
+        for pt in samp.trim().split(',') {
+          if let Some((num, color)) = pt.trim().split_once(' ') {
+            let v = num.parse().unwrap();
+            match color {
+              "red" => sample.x = v,
+              "green" => sample.y = v,
+              "blue" => sample.z = v,
+              _ => {}
+            }
+          }
+        }
+        sample
+      }).collect(),
+    };
+    games.push(game);
+  }
+  games
 }
 
 fn puzzle1() {
-    let games = load_data();
-    let fix = UVec3::new(12, 13, 14);
-    let sum = games.iter()
-        .filter(|game| game.samples.iter()
-            .all(|pt| pt.x <= fix.x && pt.y <= fix.y && pt.z <= fix.z))
-        .fold(0, |acc, game| game.id + acc);
-    println!("puzzle 1: {:?}", sum);
+  let games = load_data();
+  let fix = UVec3::new(12, 13, 14);
+  let sum = games.iter()
+    .filter(|game| game.samples.iter()
+      .all(|pt| pt.x <= fix.x && pt.y <= fix.y && pt.z <= fix.z))
+    .fold(0, |acc, game| game.id + acc);
+  println!("puzzle 1: {}", sum);
 }
 
 fn puzzle2() {
+  let games = load_data();
+  let sum = games.iter().fold(0, |acc, game| {
+    let max = game.samples.iter().fold(UVec3::ZERO, |acc: UVec3, sample| sample.max(acc));
+    acc + max.x * max.y * max.z
+  });
+  println!("puzzle 2: {}", sum);
 }
 
 pub fn run() {
-    puzzle1();
-    puzzle2();
+  puzzle1();
+  puzzle2();
 }
