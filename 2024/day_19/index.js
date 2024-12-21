3887ff38c7e80a3eb2660e6f8fe5037907e40320
Tobias Bocanegra <tripod@adobe.com>
2024-12-21 11:44:43 +0100

    day 19

--- 2024/day_19/index.js
+++ 2024/day_19/index.js
@@ -1,73 +1,63 @@
-import assert from 'assert';
 import fs from 'fs';
 
 function parse() {
   let [towels, designs] = fs.readFileSync('./input.txt', 'utf-8').trim().split('\n\n');
   towels = towels.split(',')
     .map((c) => c.trim())
-    .filter((c) => !!c)
-    .map((c) => c.split(''));
+    .filter((c) => !!c);
   designs = designs.split('\n')
     .map((c) => c.trim())
-    .filter((c) => !!c)
-    .map((c) => c.split(''));
+    .filter((c) => !!c);
 
-  const root = {
-  };
-  for (const t of towels) {
-    let n = root;
-    for (const c of t) {
-      let next = n[c] ?? {};
-      n[c] = next;
-      n = next;
-    }
-    n.end = true;
-  }
+  towels.sort((a, b) => a.length - b.length);
   return {
-    root,
     towels,
     designs,
   }
 }
 
-function solve(d, dict, idx, node) {
-  if (idx === d.length) {
-    // if we reached the end of the design return if the towel also ends
-    return node.end || node === dict;
-  }
-  const color = d[idx++];
-  const next = node[color];
-  if (next) {
-    const ret = solve(d, dict, idx, next);
-    if (ret) {
-      return true;
+function solve(design, ts) {
+  const q = [];
+  q.push({
+    d: design,
+    s: 1,
+  });
+
+  let count = 0;
+  while (q.length) {
+    const { d, s } = q.shift();
+    for (const t of ts) {
+      if (d === t) {
+        count += s;
+      } else if (d.startsWith(t)) {
+        const sd = d.substring(t.length);
+        const ep = q.find((e) => e.d === sd);
+        if (ep) {
+          ep.s += s;
+        } else {
+          q.push({
+            d: sd,
+            s,
+          })
+        }
+      }
     }
   }
-
-  // no pattern found for the next, and this towel ended, also try starting from the beginning
-  if (node.end && dict[color]) {
-    return solve(d, dict, idx, dict[color]);
-  }
-  return false;
+  return count;
 }
 
 function puzzle1() {
-  const { root, towels, designs } = parse();
-  console.log(towels.length, designs.length);
-  // console.log(JSON.stringify(root, null, 2 ));
-  // sort out the designs that where the patterns never occur
+  let { towels, designs } = parse();
   let n = 0;
+  let sum = 0;
   for (const d of designs) {
-    const combs = solve(d, root, 0, root);
-    console.log(d.join(''), combs);
-    n += combs ? 1 : 0;
+    const count = solve(d, towels);
+    if (count) {
+      sum += count;
+      n += 1;
+    }
   }
-  return n;
+  return [n, sum];
 }
 
-
-function puzzle2() {
-}
-
-console.log('puzzle 1: ', puzzle1());
-console.log('puzzle 2: ', puzzle2());
+console.log('puzzle 1,2: ', puzzle1()); // 336, 758890600222015
